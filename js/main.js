07c2b2b6fa55ed184c0fcc1f205468962735afc0
Lee Shavit <lee.shavit10@gmail.com>
2024-09-10 14:15:45 +0300

    Implement onBallClick() to make ball grow

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 'use strict'
 
+const increment = 50
+var gDiameter = 100
 
-function onBallClick(){
-    console.log('msg')
+function onBallClick(elBall){
+    gDiameter+= increment
+    elBall.style.height= gDiameter+'px'
+    elBall.style.width= gDiameter+'px'
+    updateDiameter()
+}
+
+function updateDiameter(){
+    const elDiameter= document.querySelector('.diameter')
+    elDiameter.innerText = gDiameter+'px'
 }
