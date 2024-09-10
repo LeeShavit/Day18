5cb7a031a8cb943967c58b28588d4813a0102cd3
Lee Shavit <lee.shavit10@gmail.com>
2024-09-10 14:43:21 +0300

    Reset ball at 400

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 const increment = 50
 var gDiameter = 100
 
-function onBallClick(elBall){
-    gDiameter+= increment
-    elBall.style.height= gDiameter+'px'
-    elBall.style.width= gDiameter+'px'
+function onBallClick(elBall) {
+    gDiameter = gDiameter < 400 ? gDiameter+increment : 100
+    elBall.style.height = gDiameter + 'px'
+    elBall.style.width = gDiameter + 'px'
     updateDiameter()
 }
 
-function updateDiameter(){
-    const elDiameter= document.querySelector('.diameter')
-    elDiameter.innerText = gDiameter+'px'
+function updateDiameter() {
+    const elDiameter = document.querySelector('.diameter')
+    elDiameter.innerText = gDiameter + 'px'
 }
