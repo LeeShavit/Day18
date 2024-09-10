05b49ded344d42e4a5f6b30ce3b3aa9e83cd0891
Lee Shavit <lee.shavit10@gmail.com>
2024-09-10 15:17:54 +0300

    Change ball increment to rand

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 'use strict'
 
-const increment = 50
+var increment = 50
 var gDiameter = 100
 
 function onBallClick(elBall) {
+    increment= getRandomInt(20,61)
     gDiameter = gDiameter < 400 ? gDiameter+increment : 100
     elBall.style.height = gDiameter + 'px'
     elBall.style.width = gDiameter + 'px'
