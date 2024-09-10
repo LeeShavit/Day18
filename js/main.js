6f27b82eb6a8f7f90586d912287a0b9f6a63a883
Lee Shavit <lee.shavit10@gmail.com>
2024-09-10 18:05:23 +0300

    Add features reduce swap reset

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 'use strict'
 
-var increment = 50
-var gDiameters = [100, 100]
-var gColors = []
+var increment
+var gDiameters
+var gColors
+var gIntervalTimer
+var intervalCount = 0
+
+function onInit() {
+    increment = 50
+    gDiameters = [100, 100]
+    gColors = ['lightblue', 'pink']
+}
 
 function onBallClick(elBall, maxDiameter, i) {
     increment = getRandomInt(20, 61)
@@ -20,10 +28,26 @@ function onBallSwap() {
         const elBall = document.querySelector(`.ball${i + 1}`)
         elBall.style.width = gDiameters[i] + 'px'
         elBall.style.height = gDiameters[i] + 'px'
-        elBall.style.backgroundColor= gColors[i]
+        elBall.style.backgroundColor = gColors[i]
         updateDiameterDOM(i)
     }
 
+}
+
+function onBallReduce() {
+    const randNum = getRandomInt(20, 61)
+    for (var i = 0; i < gDiameters.length; i++) {
+        gDiameters[i] = (gDiameters[i] - randNum < 100) ? 100 : gDiameters[i] - randNum
+        const elBall = document.querySelector(`.ball${i + 1}`)
+        elBall.style.width = gDiameters[i] + 'px'
+        elBall.style.height = gDiameters[i] + 'px'
+        updateDiameterDOM(i)
+    }
+}
+
+function onChangeBackground(color = getRandomColor()) {
+    const elPage = document.querySelector('body')
+    elPage.style.backgroundColor = color
 }
 
 function updateDiameterDOM(i) {
@@ -34,4 +58,39 @@ function updateDiameterDOM(i) {
 function changeBallRandColor(elBall, i) {
     gColors[i] = getRandomColor() + ''
     elBall.style.backgroundColor = gColors[i]
+}
+
+function onResetGame() {
+    onInit()
+    onChangeBackground('black')
+    resetBalls()
+}
+
+function resetBalls() {
+    for (var i = 0; i < gDiameters.length; i++) {
+        const elBall = document.querySelector(`.ball${i + 1}`)
+        elBall.style.backgroundColor = gColors[i]
+        elBall.style.height = gDiameters[i] + 'px'
+        elBall.style.width = gDiameters[i] + 'px'
+        updateDiameterDOM(i)
+    }
+}
+
+function onStartInterval() {
+    gIntervalTimer = setInterval(runMouseClickOnBalls, 2000)
+}
+
+function runMouseClickOnBalls() {
+    if(intervalCount>=10) clearInterval(gIntervalTimer)
+    const elBall1 = document.querySelector(`.ball1`)
+    const elBall2 = document.querySelector(`.ball2`)
+    onBallClick(elBall1, 300, 0)
+    onBallClick(elBall2, 300, 0)
+    onBallSwap()
+    onBallReduce()
+    intervalCount++
+}
+
+function onEndInterval() {
+    clearInterval(gIntervalTimer)
 }
