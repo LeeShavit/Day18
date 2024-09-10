bcce2aa6f1b2b3c3f21afa560ff0748c356a698c
Lee Shavit <lee.shavit10@gmail.com>
2024-09-10 16:19:41 +0300

    Add ball 3, onclick() swap ball1+ball2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 'use strict'
 
 var increment = 50
-var gDiameters = [100,100]
+var gDiameters = [100, 100]
+var gColors = []
 
-function onBallClick(elBall,maxDiameter,i) {
-    increment= getRandomInt(20,61)
-    gDiameters[i] = gDiameters[i] < maxDiameter ? gDiameters[i]+increment : 100
+function onBallClick(elBall, maxDiameter, i) {
+    increment = getRandomInt(20, 61)
+    gDiameters[i] = gDiameters[i] < maxDiameter ? gDiameters[i] + increment : 100
     elBall.style.height = gDiameters[i] + 'px'
     elBall.style.width = gDiameters[i] + 'px'
     updateDiameterDOM(i)
-    changeBallRandColor(elBall)
+    changeBallRandColor(elBall, i)
+}
+
+function onBallSwap() {
+    gDiameters.push(gDiameters.shift())
+    gColors.push(gColors.shift())
+    for (var i = 0; i < gDiameters.length; i++) {
+        const elBall = document.querySelector(`.ball${i + 1}`)
+        elBall.style.width = gDiameters[i] + 'px'
+        elBall.style.height = gDiameters[i] + 'px'
+        elBall.style.backgroundColor= gColors[i]
+        updateDiameterDOM(i)
+    }
+
 }
 
 function updateDiameterDOM(i) {
-    const elDiameter = document.querySelector(`.ball${i+1}`)
-    elDiameter.innerText = gDiameters[i] + 'px'
+    const elBall = document.querySelector(`.ball${i + 1}`)
+    elBall.innerText = gDiameters[i] + 'px'
 }
 
-function changeBallRandColor(elBall){
-    elBall.style.backgroundColor= getRandomColor()+''
+function changeBallRandColor(elBall, i) {
+    gColors[i] = getRandomColor() + ''
+    elBall.style.backgroundColor = gColors[i]
 }
